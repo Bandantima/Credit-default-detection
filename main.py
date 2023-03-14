a2ddf3202010d1965446ff1fce00e5c972a965dc
Bandantima <antimapatel02@gmail.com>
2023-03-14 12:09:27 +0530

    data transformation done

--- main.py
+++ main.py
@@ -1,40 +1,53 @@
 from Credit.logger import logging
 from Credit.exception import CreditException
-import os
-import sys
 from Credit.utils import get_collection_as_dataframe
+import sys, os
 from Credit.entity.config_entity import DataIngestionConfig
 from Credit.entity import config_entity
 from Credit.components.data_ingestion import DataIngestion
+from Credit.components.data_validation import DataValidation
+from Credit.components.data_transformation import DataTransformation
 
-
-'''
-def test_logger_and_expection():
-    try:
-       logging.info("Starting the test_logger_and_exception")
-       result = 3/0
-       print(result)
-       logging.info("Stoping the test_logger_and_exception")
-    except Exception as e:
-       logging.debug(str(e))
-       raise InsuranceException(e, sys)
-'''
-
-'''
-if __name__=="__main__":
-    try:
-         get_collection_as_dataframe(database_name='project',collection_name='Credit')
-    except Exception as e:
-         print(e)
-'''
+#def test_logger_and_expection():
+   # try:
+       # logging.info("Starting the test_logger_and_exception")
+        #result = 3/0
+       # print(result)
+       # logging.info("Stoping the test_logger_and_exception")
+    #except Exception as e:
+      #  logging.debug(str(e))
+       # raise CreditException(e, sys)
 
 if __name__=="__main__":
-    try:
-      training_pipeline_config = config_entity.TrainingPipelineConfig()
-       #data ingestion
-      data_ingestion_config  = config_entity.DataIngestionConfig(training_pipeline_config=training_pipeline_config)
-      print(data_ingestion_config.to_dict())
-      data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
-      data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
-    except Exception as e:
-       print(e)
+     try:
+          #start_training_pipeline()
+          #test_logger_and_expection()
+       # get_collection_as_dataframe(database_name ="INSURANCE", collection_name = 'INSURANCE_PROJECT')
+       training_pipeline_config = config_entity.TrainingPipelineConfig()
+       
+      #data ingestion
+       data_ingestion_config  = config_entity.DataIngestionConfig(training_pipeline_config=training_pipeline_config)
+       print(data_ingestion_config.to_dict())
+       data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
+       data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+       
+      
+
+      #Data Transformation
+
+       data_transformation_config = config_entity.DataTransformationConfig(training_pipeline_config=training_pipeline_config)
+       data_transformation = DataTransformation(data_transformation_config=data_transformation_config, 
+       data_ingestion_artifact=data_ingestion_artifact)
+       data_transformation_artifact = data_transformation.initiate_data_transformation()
+
+ 
+      # Data Validation
+       data_validation_config = config_entity.DataValidationConfig(training_pipeline_config=training_pipeline_config)
+       data_validation = DataValidation(data_validation_config=data_validation_config,
+                         data_ingestion_artifact=data_ingestion_artifact)
+        
+       data_validation_artifact = data_validation.initiate_data_validation()
+
+            
+     except Exception as e:
+          raise CreditException(e, sys)
